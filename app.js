2fab399ef792d9bbb13472b8c3e69b4cbf24a747
Aly Hussein <111089237+alyHusseinn@users.noreply.github.com>
2024-02-12 17:14:26 +0200

    preparing for deployment

--- app.js
+++ app.js
@@ -6,9 +6,16 @@ var logger = require("morgan");
 const mongoose = require("mongoose");
 const { MONGODB_URL, PORT } = require("./utils/config");
 const log = require("./utils/logger");
+const compression = require("compression");
 
 var indexRouter = require("./routes/index");
 const catalogRouter = require("./routes/catalog");
+// Set up rate limiter: maximum of twenty requests per minute
+const RateLimit = require("express-rate-limit");
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 20,
+});
 
 var app = express();
 
@@ -23,11 +30,13 @@ mongoose.connect(MONGODB_URL).then(() => {
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
+app.use(compression());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(limiter);
 
 app.use("/", indexRouter);
 app.use("/catalog", catalogRouter);
