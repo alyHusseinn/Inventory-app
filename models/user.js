7c500f6aeee51fec19a31538477517c28e40f88b
Aly Hussein <111089237+alyHusseinn@users.noreply.github.com>
2024-02-14 13:06:12 +0200

    add the user schema

---
+++ models/user.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    hash: {
+      type: String,
+      required: true,
+    },
+    salt: {
+      type: String,
+      required: true,
+    },
+  },
+});
+
+userSchema.virtual("url").get(function(){
+    return `/catalog/user/${this_id}`;
+});
+
+module.exports = mongoose.model("User", userSchema);
