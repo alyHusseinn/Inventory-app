10b087e4f727e574ad86cb8fd561a32828de2bc9
Aly Hussein <111089237+alyHusseinn@users.noreply.github.com>
2024-02-29 23:42:37 +0200

    update user view

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,16 +1,22 @@
 const User = require("../models/user");
 const Song = require("../models/song");
+const Genre = require("../models/genre");
+const Artist = require("../models/artist");
 const asyncHandler = require("express-async-handler");
 
 exports.user_details = asyncHandler(async (req, res, next) => {
-  const [user, songs] = await Promise.all([
+  const [user, songs, artists, genres] = await Promise.all([
     User.findById(req.params.id).exec(),
     Song.find({ created_by: req.params.id }).exec(),
+    Artist.find({ created_by: req.params.id }).exec(),
+    Genre.find({ created_by: req.params.id }).exec(),
   ]);
 
   res.render("user_details", {
     title: `${user.username} profile`,
     user: user,
     songs: songs,
+    artists: artists,
+    genres: genres,
   });
 });
